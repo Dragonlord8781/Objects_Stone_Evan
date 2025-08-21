768ef46695cdbbbc8a2f16ab49d8ec6e51952e76
Dragonlord8781 <evan.stone638@gmail.com>
2025-08-21 10:45:59 -0500

    8/21/25
    Video 16:41 to finish

---
+++ Objects_Stone_Evan/Computer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_Stone_Evan
+{
+    class Computer : IBootUp
+    {
+
+        public bool IsOn { get; set; }
+
+        public string Brand {  get; set; }  
+
+        public string Type { get; set; }    
+
+        public Computer(string brand, string type, bool isOn) 
+        {
+            Brand = brand;
+            Type = type;
+            isOn = isOn;
+        }
+
+        public void PowerOnOff()
+        {
+            IsOn = !IsOn;
+
+            if (IsOn == true) 
+            {
+                Console.WriteLine("The computer is booting up!");
+            }
+            else
+            {
+                Console.WriteLine("The Computer is shutting down!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"The Computer is made by {Brand} and is a {Type} computer!";
+        }
+    }
+}
--- Objects_Stone_Evan/Program.cs
+++ Objects_Stone_Evan/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Objects_Stone_Evan;
+
+Console.WriteLine("Program 17");
+
+Computer myComputer = new Computer("Apple", "All-in-One", true);
+IBootUp myBootUp = myComputer;
+Console.WriteLine(myComputer.Equals(myBootUp)); 
+
+Computer myOtherComputer = new Computer("Apple", "All-in-One", true);
+Console.WriteLine(myComputer.Equals(myOtherComputer));
+
+string myString = myComputer.ToString();
+Console.WriteLine(myString == myComputer.ToString());
+Console.WriteLine(myString == myOtherComputer.ToString());
+
+Console.WriteLine(myComputer.GetType());
+Console.WriteLine(myBootUp.GetType());
+
+myOtherComputer.PowerOnOff();
+
+if
